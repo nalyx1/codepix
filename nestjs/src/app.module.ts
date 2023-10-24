fbe3c2b24ac047843c801dc10570a996f9c1a4af
João Rodrigues <joojrodrigues.dev@outlook.com>
2023-10-23 21:11:49 -0300

    feat: typeorm bank account entity config

--- nestjs/src/app.module.ts
+++ nestjs/src/app.module.ts
@@ -2,9 +2,22 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { BankAccountsModule } from './bank-accounts/bank-accounts.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [BankAccountsModule],
+  imports: [
+    BankAccountsModule,
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: 'db',
+      port: 5432,
+      username: 'postgres',
+      password: 'postgres',
+      database: 'nest',
+      autoLoadEntities: true,
+      synchronize: true,
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
